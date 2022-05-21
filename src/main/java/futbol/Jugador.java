40be825d0744fbbbf6cb394702b840e8ca0580fd
JeancoMontoyaC <92815992+JeanCarloMontoya@users.noreply.github.com>
2022-05-21 17:25:53 -0500

    ds

--- src/main/java/futbol/Jugador.java
+++ src/main/java/futbol/Jugador.java
@@ -1,5 +1,57 @@
 package futbol;
 
-abstract class Jugador {
+
+public class Jugador extends Futbolista   {
+	public short golesMarcados;
+	public byte dorsal;
+	public Jugador(String nombre, int edad, short golesMarcados, byte dorsal) {
+		super(nombre, edad, "Delantero");
+		this.golesMarcados = golesMarcados;
+		this.dorsal = dorsal;
+	}
+	
+	public short getGolesMarcados() {
+		return golesMarcados;
+	}
+
+	public void setGolesMarcados(short golesMarcados) {
+		this.golesMarcados = golesMarcados;
+	}
+
+	public byte getDorsal() {
+		return dorsal;
+	}
+
+	public void setDorsal(byte dorsal) {
+		this.dorsal = dorsal;
+	}
+
+	public Jugador() {
+		golesMarcados=298;
+		dorsal=7;
+	}
+	
+	public String toString() {
+		return "El futbolista "+this.getNombre()+" tiene "+this.getEdad()+ ", y juega de "+this.getPosicion()
+		+" con el dorsal "+dorsal+ ". Ha marcado "+golesMarcados;
+	}
+	public boolean jugarConLasManos() {
+		if(this instanceof Futbolista) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	public int compareTo(Futbolista o) {
+		return(java.lang.Math.abs(o.getEdad()-this.getEdad()));
+	}
+	
+	
+	
+	
+	
+	
+	
 
 }
--- src/main/java/futbol/Portero.java
+++ src/main/java/futbol/Portero.java
@@ -1,5 +1,24 @@
 package futbol;
 
-public class Portero {
-
+public class Portero extends Futbolista{
+	public short golesRecibidos;
+	public byte dorsal;
+	public Portero(String nombre, int edad, short golesRecibidos, byte dorsal) {
+		super(nombre, edad, "Portero");
+		this.golesRecibidos = golesRecibidos;
+		this.dorsal = dorsal;
+	}
+	public boolean jugarConLasManos() {
+		return true;
+	}
+	public int compareTo(Futbolista o) {
+		return java.lang.Math.abs(((Portero)o).golesRecibidos-this.golesRecibidos);
+		
+	}
+	public String toString() {
+		return "El futbolista "+this.getNombre()+
+				" tiene "+this.getEdad()+ ", y juega de "+this.getPosicion()+
+				" con el dorsal "+dorsal+ ".Le han marcado"+golesRecibidos;
+	}
+	
 }
